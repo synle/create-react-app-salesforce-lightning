90a6382c2017eea2cafca0f379659c70c46cc47f
Sy Le <lenguyensy@gmail.com>
2019-04-05 12:38:53 -0700

    salesforce lightning - package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Illustration from '@salesforce/design-system-react/components/illustration';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Illustration
+            heading="Boostraping create-react-app 2x with Salesforce Lightning Design System"
+            messageBody={<span>Step-by-step instruction can be found at <a href="https://github.com/synle/create-react-app-salesforce-lightning">this tutorial guide</a></span>}
+            path="/assets/images/illustrations/empty-state-no-access.svg#no-access"
+            size="large"
+          />
         </header>
       </div>
     );

c674e7ed18d23945a6aa27fc2496394482f8b828
Sy Le <lenguyensy@gmail.com>
2022-02-07 16:07:02 -0800

    update packages and added prettier

--- config-overrides.js
+++ config-overrides.js
@@ -5,23 +5,16 @@ module.exports = function override(config, env) {
     // salesforce dependencies
     // this will compile salesforce lightning as src, not as package
     {
-        test: /\.jsx?$/,
-        include: [
-          'node_modules/@salesforce/design-system-react',
-        ].map(
-          someDir => path.resolve(
-            process.cwd(),
-            someDir
-          )
-        ),
-        loader: require.resolve('babel-loader'),
-        options: {
-          presets: [
-            "react-app"
-          ],
-        },
+      test: /\.jsx?$/,
+      include: ['node_modules/@salesforce/design-system-react'].map((someDir) =>
+        path.resolve(process.cwd(), someDir),
+      ),
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: ['react-app'],
+      },
     },
   ].concat(config.module.rules);
 
   return config;
-}
+};
--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Illustration from '@salesforce/design-system-react/components/illustration';
 
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        <header className='App-header'>
           <Illustration
-            heading="Boostraping create-react-app 2x with Salesforce Lightning Design System"
-            messageBody={<span>Step-by-step instruction can be found at <a href="https://github.com/synle/create-react-app-salesforce-lightning">this tutorial guide</a></span>}
-            path="/assets/images/illustrations/empty-state-no-access.svg#no-access"
-            size="large"
+            heading='Boostraping create-react-app 2x with Salesforce Lightning Design System'
+            messageBody={
+              <span>
+                Step-by-step instruction can be found at{' '}
+                <a href='https://github.com/synle/create-react-app-salesforce-lightning'>
+                  this tutorial guide
+                </a>
+              </span>
+            }
+            path='/assets/images/illustrations/empty-state-no-access.svg#no-access'
+            size='large'
           />
         </header>
       </div>
